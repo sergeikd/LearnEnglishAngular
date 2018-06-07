45727d97a2dafdd2e1a45bc08c0ea038959fafaa
ITRANSITION\s2.kudryavtsev <s2.kudryavtsev@itransition.com>
2018-06-07 18:34:22 +0300

    Default MVC template

---
+++ LearnEnglishAngular/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace LearnEnglishAngular.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ LearnEnglishAngular/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace LearnEnglishAngular.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
